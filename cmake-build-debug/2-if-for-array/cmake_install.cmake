054160c18091d654d16c35ad5db868b520c0f1d4
“wbh17” <3054912460@qq.com>
2023-10-07 21:02:36 +0800

    2

--- cmake-build-debug/2-if-for-array/cmake_install.cmake
+++ cmake-build-debug/2-if-for-array/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: E:/codes/cpl-study
+# Install script for directory: E:/codes/cpl-study/2-if-for-array
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -37,28 +37,3 @@ if(NOT DEFINED CMAKE_OBJDUMP)
   set(CMAKE_OBJDUMP "E:/software/CLion 2023.2.2/bin/mingw/bin/objdump.exe")
 endif()
 
-if(NOT CMAKE_INSTALL_LOCAL_ONLY)
-  # Include the install script for the subdirectory.
-  include("E:/codes/cpl-study/cmake-build-debug/0-intro/cmake_install.cmake")
-endif()
-
-if(NOT CMAKE_INSTALL_LOCAL_ONLY)
-  # Include the install script for the subdirectory.
-  include("E:/codes/cpl-study/cmake-build-debug/1-types-io/cmake_install.cmake")
-endif()
-
-if(NOT CMAKE_INSTALL_LOCAL_ONLY)
-  # Include the install script for the subdirectory.
-  include("E:/codes/cpl-study/cmake-build-debug/2-if-for-array/cmake_install.cmake")
-endif()
-
-if(CMAKE_INSTALL_COMPONENT)
-  set(CMAKE_INSTALL_MANIFEST "install_manifest_${CMAKE_INSTALL_COMPONENT}.txt")
-else()
-  set(CMAKE_INSTALL_MANIFEST "install_manifest.txt")
-endif()
-
-string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
-       "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "E:/codes/cpl-study/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
-     "${CMAKE_INSTALL_MANIFEST_CONTENT}")
