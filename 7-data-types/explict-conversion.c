a033dd5c8d8ea0f66abc46087d9827c2fbfc201e
“wbh17” <3054912460@qq.com>
2023-11-19 21:47:28 +0800

    7

---
+++ 7-data-types/explict-conversion.c
@@ -0,0 +1,20 @@
+//
+// Created by hfwei on 2022/11/10.
+//
+
+#include <stdio.h>
+#include <limits.h>
+
+int main() {
+    double pi = 3.14159;
+
+    // below: obtain its fractional part
+    double fraction = pi - (int) pi;
+
+    int num = 1000000000; // (10 digits)
+    printf("LLONG_MAX = %lld\n", LLONG_MAX);
+    long long llint = (long long) num * num;
+    printf("i = %lld\n", llint);
+
+    return 0;
+}
--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -67,6 +67,16 @@ if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   include("E:/codes/cpl-study/cmake-build-debug/5-function/cmake_install.cmake")
 endif()
 
+if(NOT CMAKE_INSTALL_LOCAL_ONLY)
+  # Include the install script for the subdirectory.
+  include("E:/codes/cpl-study/cmake-build-debug/6-recursion/cmake_install.cmake")
+endif()
+
+if(NOT CMAKE_INSTALL_LOCAL_ONLY)
+  # Include the install script for the subdirectory.
+  include("E:/codes/cpl-study/cmake-build-debug/7-data-types/cmake_install.cmake")
+endif()
+
 if(CMAKE_INSTALL_COMPONENT)
   set(CMAKE_INSTALL_MANIFEST "install_manifest_${CMAKE_INSTALL_COMPONENT}.txt")
 else()
